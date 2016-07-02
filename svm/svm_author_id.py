299c3b98c0df4a5b4894e9d3596b52bb64e5b40d
Shantanu Goel <shantanu.goel@gmail.com>
2016-07-02 22:25:56 +0530

    svm

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,6 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -24,6 +26,25 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+#l = int(len(features_train)/100)
+#features_train = features_train[:l]
+#labels_train = labels_train[:l]
+clf = svm.SVC(kernel='rbf', C=10000)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print(pred[10])
+print(pred[26])
+print(pred[50])
+print(accuracy_score(labels_test, pred))
+c_count = 0
+s_count = 1
+for p in pred:
+    if p == 0:
+        s_count += 1
+    else:
+        c_count += 1
+print(c_count)
+print(s_count)
 
 #########################################################
 
